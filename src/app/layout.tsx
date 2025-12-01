249e5937584264aed9fe9700f01b78aa53f1fff8
hoang-nguyens <hoang0403ss@gmail.com>
2025-12-01 10:56:55 +0700

    Fix dark/light mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,6 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
           storageKey="hanog-blog-theme"
         >
           <div className="flex min-h-screen flex-col">
